534166a94cf8f3491ad0924102618b43f92d6626
raashed <mohammedraashed10@gmail.com>
2025-12-07 09:16:02 +0530

    feat(db): add users table migration with UUID primary key and timestamps - Define `users` table with columns: `id` (UUID primary key), `email` (unique, not null), `full_name` (not null), `password_hash` (not null), `created_at` and `updated_at` timestamps with timezone - Use `gen_random_uuid()` as default for `id` to ensure unique user identifiers - Include default current timestamp for creation and update tracking - Add SQL migration script for schema versioning and database consistency management

--- db/migrations/20251207033055_create_users_table.sql
+++ db/migrations/20251207033055_create_users_table.sql
@@ -5,6 +5,16 @@ CREATE TYPE project_status AS ENUM ('active', 'completed', 'archived')
 CREATE TYPE task_status AS ENUM ('pending', 'in_progress', 'completed', 'cancelled')
 CREATE TYPE member_role AS ENUM ('owner', 'admin', 'member')
 
+-- create user table
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email TEXT NOT NULL UNIQUE,
+    full_name TEXT NOT NULL,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+)
+
 
 -- migrate:down
 
